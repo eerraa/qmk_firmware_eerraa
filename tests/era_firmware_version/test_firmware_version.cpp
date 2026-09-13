e662e48d870fbf29883436bfefa013b7321d29a0
eerraa <phyojinbin@gmail.com>
2026-09-14 01:40:02 +0900

    test: expect the selected V260913R1 release identity

--- tests/era_firmware_version/test_firmware_version.cpp
+++ tests/era_firmware_version/test_firmware_version.cpp
@@ -61,7 +61,7 @@ void expect_handler_declines_unchanged(uint8_t command, uint8_t channel, uint8_t
 } // namespace
 
 TEST(EraFirmwareVersion, CanonicalIdentityAndCompleteNulTerminatedPayload) {
-    static constexpr char kExpected[] = "260914R1";
+    static constexpr char kExpected[] = "260913R1";
 
     EXPECT_STREQ(ERA_FIRMWARE_VERSION, kExpected);
     EXPECT_STREQ(era_firmware_version, kExpected);
